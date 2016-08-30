f85318743fa02fdb9b45a5fa749756e0466562a0
yoonjucho <cyj08070807@naver.com>
2016-08-30 19:43:07 +0900

    yoonju

--- app/src/main/java/org/androidtown/hackathon_4j/MainActivity.java
+++ app/src/main/java/org/androidtown/hackathon_4j/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     //hajeong
     //55
 
+    //yoonju
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
