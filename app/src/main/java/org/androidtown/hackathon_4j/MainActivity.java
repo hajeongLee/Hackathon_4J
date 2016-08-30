c33f8c9170e20471918f5002ca96429feafd089c
hajeongLee <kissesvv@naver.com>
2016-08-30 18:21:48 +0900

    hajeong

---
+++ app/src/main/java/org/androidtown/hackathon_4j/MainActivity.java
@@ -0,0 +1,17 @@
+package org.androidtown.hackathon_4j;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //hajeong
+    //55
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
